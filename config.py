45700c0009b8cb1aa003feea82a7ec8932e77713
Christian Wang <2936385857@qq.com>
2025-03-15 14:02:13 +0800

    Update config.py

--- config.py
+++ config.py
@@ -9,12 +9,14 @@ quiz_name = ""
 # Application settings
 headless = True  # Set to True for headless mode
 debug_output = False  # Set to True for more verbose output
-vision_model = "DOUBAO-1.5V"  # Vision model to use
-text_model = "DOUBAO-1.5-PRO"
+vision_model = "DOUBAO-1.5V"  # model to use for questions containing images
+text_model = "DOUBAO-1.5-PRO" # model to use for questions with only text
 
 # Language setting (en for English, zh for Chinese)
-language = "zh"  # Change to "zh" for Chinese
+language = "zh"  # Change to "en" for English
 
+GLM_API_KEY = "" #API key for zhipu ai
+ARK_API_KEY = "" #API key for volcano ai
 
-GLM_API_KEY = ""
-ARK_API_KEY = ""
+# Token for wechat notification (see details in release notes)
+WECHAT = ""
